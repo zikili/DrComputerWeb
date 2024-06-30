96fec4d3fe88c6402f17483d85e20008db32b752
Ziv Siag <ziv.siag@gmail.com>
2024-07-01 00:19:34 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-,
+  </React.StrictMode>,
 )
