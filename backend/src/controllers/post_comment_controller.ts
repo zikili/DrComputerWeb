cf7f71d21ef0c8e8b9cd1637835867d13ab7f6fd
Ziv Siag <ziv.siag@gmail.com>
2024-07-03 20:05:14 +0300

    Post Comment controller

--- backend/src/controllers/post_comment_controller.ts
+++ backend/src/controllers/post_comment_controller.ts
@@ -1,11 +1,26 @@
-//TODO check if works
-import PostComment, { IPostComment } from "../models/post_comment_model";
 
+import { Request, Response } from "express";
 import BaseController from "./base_controller";
+import PostComment,{ IPostComment } from "../models/post_comment_model";
 
-class UserController extends BaseController<IPostComment> {
+class PostCommentController extends BaseController<IPostComment> {
   constructor() {
     super(PostComment);
   }
+  async get(req: Request, res: Response) {
+    try{
+    if (req.params.id != null) {
+      const myObjects = await this.model.find({postId:req.params.id});
+      return res.status(200).send(myObjects);
+  } 
+    else{
+      res.status(400).send("missing id");
+    }
+  }
+  catch(error)
+  {
+    res.status(500).send(error);
+  }
 }
-export default new UserController();
+}
+export default new PostCommentController();
