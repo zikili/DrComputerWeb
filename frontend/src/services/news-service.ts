a84b82d312c41f07d20cdc9fdb20c10072519473
Ziv Siag <ziv.siag@gmail.com>
2024-07-16 14:05:14 +0300

    Article api

---
+++ frontend/src/services/news-service.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+
+
+export interface Article{
+    source: string,
+    author?: string,
+    title: string,
+    description?: string,
+    url: string,
+    urlToImage?: string,
+    publishedAt: string,
+    content?: string
+}
+export interface Data {
+    status: string,
+    totalResults: number,
+    articles: Article[]
+}
+export const getData = async ():Promise<AxiosResponse<Data>> => {
+    try {
+        const baseUrl='https://newsapi.org/v2/'
+        const key=import.meta.env.VITE_NEWS_API_KEY
+        const type="everything"
+        const query="q=computer"
+        const url= `${baseUrl}${type}/?${query}&apiKey=${key}`
+
+        const response: Promise<AxiosResponse<Data>> = axios.get(url);
+        return response
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+};
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,5 +1,6 @@
 interface ImportMetaEnv {
     VITE_API_BASE_URL: string;
+    VITE_NEWS_API_KEY: string;
     // Define other environment variables here as needed
   }
   
