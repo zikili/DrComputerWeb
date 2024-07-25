72b33ed9bb295032115dfd21ac9a41d4ed375881
Ziv Siag <ziv.siag@gmail.com>
2024-07-25 20:08:47 +0300

    Update Server.ts

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,9 +1,23 @@
 import init from "./App";
-import swaggerUI from "swagger-ui-express"
-import swaggerJsDoc from "swagger-jsdoc"
-
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import http from "http";
+import https from "https";
+import fs from "fs";
 
 init().then((app) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.log("DEVELOPMENT");
+    http.createServer(app).listen(process.env.PORT);
+  } else {
+    console.log("PRODUCTION");
+    const options2 = {
+      key: fs.readFileSync("../../client-key.pem"),
+      cert: fs.readFileSync("../client-cert.pem"),
+    };
+    https.createServer(options2, app).listen(process.env.HTTPS_PORT);
+  }
+
   const options = {
     definition: {
       openapi: "3.0.0",
@@ -12,14 +26,12 @@ init().then((app) => {
         version: "1.0.0",
         description: "REST server including authentication using JWT",
       },
-      servers: [{ url: "http://localhost:"+process.env.PORT }],
+      servers: [{ url: "http://localhost:" + process.env.PORT }],
     },
     apis: ["./src/routes/*.ts"],
   };
   const specs = swaggerJsDoc(options);
   app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
- 
-
 
   app.listen(process.env.PORT, () => {
     console.log(
