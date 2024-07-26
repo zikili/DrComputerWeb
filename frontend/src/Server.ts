0981a35b339112a4716b3809b274557688e7338d
Ziv Siag <ziv.siag@gmail.com>
2024-07-26 20:45:10 +0300

    try

--- frontend/src/Server.ts
+++ frontend/src/Server.ts
@@ -8,29 +8,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
-// Use express.static to serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
-console.log(path.join(__dirname, 'build'));
 
-// Handle all other routes by sending the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// HTTPS options including certificate and key
 const options = {
   key: readFileSync(path.join(__dirname, "../../client-key.pem")),
   cert: readFileSync(path.join(__dirname, "../../client-cert.pem")),
 };
 
-// Create an HTTPS server
 const httpsServer = createServer(options, app);
 
-// Define the port number
 const port = 443;
 
-// Start the HTTPS server
 httpsServer.listen(port, () => {
   console.log(`Server is running on https://localhost:${port}`);
 });
