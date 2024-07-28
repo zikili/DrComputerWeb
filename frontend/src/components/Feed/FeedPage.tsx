7388cf4aa6642c7333d276932f177fa84f3a4103
Ziv Siag <ziv.siag@gmail.com>
2024-07-28 12:32:46 +0300

    Update FeedPage.tsx

--- frontend/src/components/Feed/FeedPage.tsx
+++ frontend/src/components/Feed/FeedPage.tsx
@@ -1,47 +1,40 @@
-import  { useState, useEffect,useRef } from 'react';
-import PostService, { IPost } from '../../services/post-service'; // Adjust the path as per your project structure
-import {  AxiosResponse } from 'axios';
-import './FeedPage.css';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useRef } from "react";
+import PostService, { IPost } from "../../services/post-service"; // Adjust the path as per your project structure
+import axios, { AxiosResponse } from "axios";
+import "./FeedPage.css";
+import { useNavigate } from "react-router-dom";
 
 function FeedPage() {
   const navigate = useNavigate();
   const [posts, setPosts] = useState<IPost[]>([]);
-  const [isLoading,setIsLoading]=useState(false)
-  const [error, setError] = useState<string|null>(null);
- // Prevent state update on unmounted component
-  const cancelRef=useRef<(() => void | undefined) | undefined>();
-  if(cancelRef.current)
-    cancelRef.current()
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  // Prevent state update on unmounted component
+  const cancelRef = useRef<(() => void | undefined) | undefined>();
+  if (cancelRef.current) cancelRef.current();
   useEffect(() => {
     const fetchPosts = async () => {
-          try {
-            
-            setIsLoading(true);
-               PostService.getAll().then(async (res)=>{
-                cancelRef.current=res.cancel
-                const response: AxiosResponse<IPost[]> =  await res.req;
-                setPosts(response.data);
-              }); // Await for the getAll() result
-          } catch (error) {
-                setError("Error fetching posts")
-                  console.error('Error fetching posts:', error); 
-          }
-          finally {
-            setIsLoading(false);
-          }
-        }
-        const timerId = setTimeout(() => {
-          fetchPosts();
-        }, 500);
-    
-          return () => {
-            clearTimeout(timerId);
-            console.log("hello")
-            if(cancelRef.current)
-            cancelRef.current() // Prevent state update on destructed component
-          };
-
+      try {
+        setIsLoading(true);
+        PostService.getAll().then(async (res) => {
+          cancelRef.current = res.cancel;
+          const response: AxiosResponse<IPost[]> = await res.req;
+          setPosts(response.data);
+        }); // Await for the getAll() result
+      } catch (error) {
+        if (axios.isCancel(error))
+          console.log("Request canceled:", error.message);
+        setError("Error fetching posts");
+        console.error("Error fetching posts:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPosts();
+    return () => {
+      console.log("canceling");
+      if (cancelRef.current) cancelRef.current(); // Prevent state update on destructed component
+    };
   }, []); // Include cancelToken in dependencies to handle cleanup correctly
 
   const handlePostClick = (postId: string) => {
@@ -56,12 +49,17 @@ function FeedPage() {
       ) : error ? (
         <div className="alert alert-danger">{error}</div>
       ) : posts?.length === 0 ? (
-        <div className="no-posts"><h3>No posts have been uploaded.</h3></div>
+        <div className="no-posts">
+          <h3>No posts have been uploaded.</h3>
+        </div>
       ) : (
-        
         <div className="posts-list">
           {posts.map((post, index) => (
-            <div key={index} className="post-item" onClick={() => handlePostClick(post._id!)}>
+            <div
+              key={index}
+              className="post-item"
+              onClick={() => handlePostClick(post._id!)}
+            >
               <h2>{post.type}</h2>
               <p>GPU: {post.gpu}</p>
               <p>CPU: {post.cpu}</p>
@@ -72,7 +70,7 @@ function FeedPage() {
               <img src={post.image} alt="Post Image" />
             </div>
           ))}
-          </div>
+        </div>
       )}
     </div>
   );
